51b4b282b09d6e3d850b88a9df6f3db739b34b4d
Anh Duong <anhduong2005.gialai@gmail.com>
2025-11-09 09:06:40 +0700

    adjust preprocess

--- src/App.java
+++ src/App.java
@@ -1,9 +1,5 @@
-
 import preprocess.Preprocessor;
-import weka.classifiers.Evaluation;
 import weka.classifiers.meta.FilteredClassifier;
-import weka.classifiers.trees.J48;
-import weka.core.Debug.Random;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 
@@ -15,8 +11,5 @@ public class App {
         data.setClassIndex(data.numAttributes() - 1);
         FilteredClassifier fc = new FilteredClassifier();
         fc.setFilter(Preprocessor.createPreprocessor());
-        fc.setClassifier(new J48());
-        
-     
     }
 }
