ada5efbee49199565f2674a493231ea624f0216e
QUANG HUY <quanghuy0101vn@gmail.com>
2025-11-21 16:12:46 +0700

    Create model selection to choose the best model by AUC and tune threshold

---
+++ src/Model_Selection/RunModelSelection.java
@@ -0,0 +1,15 @@
+package Model_Selection;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils;
+
+public class RunModelSelection {
+    public static void main(String[] args) throws Exception {
+        // Load Data
+        ConverterUtils.DataSource src = new ConverterUtils.DataSource("data/data.arff");
+        Instances data = src.getDataSet();
+        data.setClassIndex(data.numAttributes() - 1);
+
+        // Run Model + threshold
+        ModelSelector.selectBestModel(data);
+    }
+}
---
+++ src/Model_Selection/ThresholdHelper.java
@@ -0,0 +1,41 @@
+package Model_Selection;
+import weka.classifiers.Evaluation;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.Utils;
+import weka.classifiers.evaluation.ThresholdCurve;
+
+public class ThresholdHelper {
+    public static double findBestThreshold(Evaluation eval, int pos) {
+        ThresholdCurve tc = new ThresholdCurve();
+        Instances curve = tc.getCurve(eval.predictions());
+
+        double bestF1 = -1;
+        double bestT = 0.5;
+
+        for (int i = 0; i < curve.numInstances(); i++) {
+            Instance point = curve.instance(i);
+
+            double precision = point.value(curve.attribute(ThresholdCurve.PRECISION_NAME));
+            double recall = point.value(curve.attribute(ThresholdCurve.RECALL_NAME));
+            double thres = point.value(curve.attribute((ThresholdCurve.THRESHOLD_NAME)));
+            double f1 = 2 * precision * recall / (precision + recall + 1e-10);
+
+            if (f1 > bestF1) {
+                bestF1 = f1;
+                bestT = thres;
+            }
+        }
+
+        System.out.println("Best threshold found: " + bestT + " with F1 = " + bestF1);
+        return bestT;
+    }
+
+    public static int predictWithThreshold(double[] probs, double threshold) {
+        if (probs == null || probs.length < 2) {
+            throw new IllegalArgumentException("Probs array must have at least 2 elements");
+        }
+
+        return probs[1] >= threshold ? 1:0;
+    }
+}
